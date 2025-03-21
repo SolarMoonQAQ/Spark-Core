f97de6506d6e687f28fc08e6a84731fcc0a4cb8c
甜粽子 <1368003448@qq.com>
2025-03-21 20:26:12 +0800

    可能修复了方块数量爆炸的问题

--- src/main/kotlin/cn/solarmoon/spark_core/physics/level/PhysicsLevel.kt
+++ src/main/kotlin/cn/solarmoon/spark_core/physics/level/PhysicsLevel.kt
@@ -12,6 +12,7 @@ import com.jme3.bullet.objects.PhysicsRigidBody
 import com.jme3.math.Vector3f
 import kotlinx.coroutines.*
 import net.minecraft.core.BlockPos
+import net.minecraft.world.entity.Entity
 import net.minecraft.world.level.ChunkPos
 import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.state.BlockState
@@ -45,7 +46,6 @@ abstract class PhysicsLevel(
     val terrainChunks: ConcurrentHashMap<ChunkPos, ChunkAccess> = ConcurrentHashMap(32) //已加载的区块
     val terrainBlocks: ConcurrentHashMap<BlockPos, BlockState> = ConcurrentHashMap(1024) //用于碰撞检测的地形块位置表
     val terrainBlockBodies: ConcurrentHashMap<BlockPos, PhysicsRigidBody> = ConcurrentHashMap(1024) //已存在的地形块
-//    private val defaultShape = BoxCollisionShape(0.5f) //默认碰撞形状
 
     @OptIn(DelicateCoroutinesApi::class, ExperimentalCoroutinesApi::class)
     private val physicsDispatcher = newSingleThreadContext(name).apply {
@@ -168,14 +168,14 @@ abstract class PhysicsLevel(
         val boundingBox = pco.boundingBox(null)
         val min = boundingBox.getMin(null)
         val max = boundingBox.getMax(null)
-        //TODO:会导致方块碰撞数量爆炸，需排查原因
-//        if (pco is PhysicsRigidBody) {
-//            val v = pco.getLinearVelocity(null)//对于移动物体，额外向速度方向延伸判定区
-//            if (v.lengthSquared() < 1600)
-//            if (v.x < 0) min.x += v.x * 0.05f else max.x += v.x * 0.05f
-//            if (v.y < 0) min.y += v.y * 0.05f else max.y += v.y * 0.05f
-//            if (v.z < 0) min.z += v.z * 0.05f else max.z += v.z * 0.05f
-//        }
+        if (pco is PhysicsRigidBody) {
+            val v = pco.getLinearVelocity(null)//对于移动物体，额外向速度方向延伸判定区
+            if (v.lengthSquared() < 1600) {
+                if (v.x < 0) min.x += v.x * 0.05f else max.x += v.x * 0.05f
+                if (v.y < 0) min.y += v.y * 0.05f else max.y += v.y * 0.05f
+                if (v.z < 0) min.z += v.z * 0.05f else max.z += v.z * 0.05f
+            }
+        }
         addTerrainBlocksToWorld(min, max)
     }
 
@@ -202,9 +202,9 @@ abstract class PhysicsLevel(
                                 // 如果块不是空气或可替换方块，记录方块的状态和坐标
                                 terrainBlocks[blockPos] = blockState
                                 submitTask {
-                                    //TODO:根据方块形状不同取不同的CollisionShape，或许预先在BlockState中建好Shape以减少资源消耗
-                                    val blockBody = PhysicsRigidBody(mcLevel, blockState.getBulletCollisionShape(), blockPos)
-                                    blockBody.setUserIndex(40) //设定销毁倒计时(2秒，120物理tick)
+                                    val blockBody =
+                                        PhysicsRigidBody(mcLevel, blockState.getBulletCollisionShape(), blockPos)
+                                    blockBody.setUserIndex(40) //设定销毁倒计时(2秒，40主线程tick)
                                     blockBody.setPhysicsLocation(
                                         Vector3f(
                                             blockPos.x.toFloat() + 0.5f,
