dc86018d51a0eb7b5d225e4c2338dd9f0f7c12a7
曦月 <3213382746@qq.com>
2025-08-04 11:33:53 +0800

    - 优化skillConfig逻辑

--- src/main/kotlin/cn/solarmoon/spark_core/skill/SkillConfig.kt
+++ src/main/kotlin/cn/solarmoon/spark_core/skill/SkillConfig.kt
@@ -2,31 +2,41 @@ package cn.solarmoon.spark_core.skill
 
 open class SkillConfig(
     val skill: Skill
-): LinkedHashMap<String, Any>() {
+) {
 
-    var ignoreAttackSpeed = false
-    var canSweepAttack = true
-    var canCriticalHit = true
-    var canTargetKnockBack= true
-    var damageMultiplier = 1f
+    val storage = linkedMapOf<String, Any>()
 
     open fun init() {
         skill.onEvent<SkillEvent.PlayerGetAttackStrength> {
-            if (ignoreAttackSpeed) it.event.attackStrengthScale = 1f
+            if (read("ignore_attack_speed", false)) it.event.attackStrengthScale = 1f
         }
         skill.onEvent<SkillEvent.SweepAttack> {
-            if (!canSweepAttack) it.event.isSweeping = false
+            if (!read("can_sweep_attack", true)) it.event.isSweeping = false
         }
         skill.onEvent<SkillEvent.CriticalHit> {
-            if (!canCriticalHit) {
+            if (!read("can_critical_hit", true)) {
                 if (it.event.vanillaMultiplier == 1.5f) it.event.isCriticalHit = false
             }
         }
         skill.onEvent<SkillEvent.TargetKnockBack> {
-            if (!canTargetKnockBack) it.event.isCanceled = true
+            if (!read("can_target_knockback", true)) it.event.isCanceled = true
         }
         skill.onEvent<SkillEvent.TargetHurt> {
+            val damageMultiplier = read("damage_multiplier", 1f)
             if (damageMultiplier != 1f) it.event.container.newDamage *= damageMultiplier
+        }
+    }
+
+    fun put(id: String, value: Any) {
+        storage[id] = value
+    }
+
+    inline fun <reified T: Any> read(key: String, defaultValue: T): T {
+        val value = storage.get(key)
+        if (value == null) return defaultValue
+        return when (value) {
+            is T -> value
+            else -> throw IllegalArgumentException("技能配置参数[$key] 的类型必须为 ${T::class.simpleName}")
         }
     }
 
