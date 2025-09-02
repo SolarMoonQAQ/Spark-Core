8d1f05c7bc212658d0772528c4e5e4447d889b4e
曦月 <3213382746@qq.com>
2025-09-03 03:38:40 +0800

    - init

---
+++ src/main/java/cn/solarmoon/spark_core_graavljs/SparkCoreGraalJS.java
@@ -0,0 +1,16 @@
+package cn.solarmoon.spark_core_graavljs;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+
+@Mod(SparkCoreGraalJS.MOD_ID)
+public class SparkCoreGraalJS {
+
+    public static final String MOD_ID = "spark_core_graaljs";
+
+    public SparkCoreGraalJS(IEventBus modEventBus, ModContainer modContainer) {
+
+    }
+
+}
