026b623b6070f85dc1430a76c14b44ea7bf9651a
甜粽子 <1368003448@qq.com>
2025-10-01 05:13:46 +0800

    添加一些便于java调用的工具方法

--- src/main/kotlin/cn/solarmoon/spark_core/physics/PhysicsHost.kt
+++ src/main/kotlin/cn/solarmoon/spark_core/physics/PhysicsHost.kt
@@ -1,8 +1,11 @@
 package cn.solarmoon.spark_core.physics
 
+import cn.solarmoon.spark_core.physics.body.owner
 import cn.solarmoon.spark_core.physics.level.PhysicsLevel
 import cn.solarmoon.spark_core.sync.Syncer
 import com.jme3.bullet.collision.PhysicsCollisionObject
+import com.jme3.bullet.collision.shapes.CollisionShape
+import com.jme3.bullet.objects.PhysicsRigidBody
 import net.minecraft.world.level.Level
 
 interface PhysicsHost {
@@ -11,8 +14,36 @@ interface PhysicsHost {
 
     val allPhysicsBodies: MutableMap<String, PhysicsCollisionObject>
 
-    fun getPhysicsBody(name: String): PhysicsCollisionObject? {
-        return allPhysicsBodies[name]
+    fun getPhysicsBody(uuid: String): PhysicsCollisionObject? {
+        return allPhysicsBodies[uuid]
     }
+
+    fun createPhysicsBody(body: PhysicsCollisionObject): PhysicsCollisionObject {
+        body.owner = this
+        return body
+    }
+
+    fun createPhysicsBody(shape: CollisionShape, mass: Float): PhysicsRigidBody {
+        val body = PhysicsRigidBody(shape, mass)
+        body.owner = this
+        return body
+    }
+
+    fun removePhysicsBody(body: PhysicsCollisionObject) {
+        physicsLevel.apply {
+            submitImmediateTask {
+                world.removeCollisionObject(body)
+            }
+        }
+    }
+
+    fun addPhysicsBody(body: PhysicsCollisionObject) {
+        physicsLevel.apply {
+            submitImmediateTask {
+                world.addCollisionObject(body)
+            }
+        }
+    }
+
 
 }
