d49dab942fd451a3bdc778418e4513d400d4f4b5
曦月 <3213382746@qq.com>
2026-06-27 05:59:24 +0800

    移除错误文件

--- minecraft/neoforge/1.21.1/src/main/java/cn/solarmoon/sparkcore/mixin/MixinConfig.java
+++
@@ -1,7 +0,0 @@
-package cn.solarmoon.sparkcore.mixin;
-
-import com.llamalad7.mixinextras.platform.neoforge.MixinExtrasConfigPlugin;
-
-public class MixinConfig extends MixinExtrasConfigPlugin {
-
-}
--- minecraft/neoforge/1.21.1/src/main/kotlin/cn/solarmoon/sparkcore/minecraft/util/ClassTypeConverter.kt
+++
@@ -1,2 +0,0 @@
-package cn.solarmoon.sparkcore.minecraft.util
-
